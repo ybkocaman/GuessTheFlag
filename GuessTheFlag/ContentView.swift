26bb4aeb52a890ad09c52ec80a60ad1bf1df5d26
Yusuf <ybkocaman@gmail.com>
2023-11-09 15:42:37 +0000

    Add Flag Animations

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,50 +9,113 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var score = 0
+    @State private var questionNumber = 0
+    
+    @State private var scoreTitle = ""
+    @State private var message = ""
+    @State private var showingScore = false
+    @State private var isGameOver = false
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        ZStack {
+//            RadialGradient(stops: [
+//                .init(color: Color(red: 0.2, green: 0.4, blue: 0.7), location: 0.3),
+//                .init(color: Color(red: 0.6, green: 0.2, blue: 0.2), location: 0.3)
+//            ], center: .top, startRadius: 200, endRadius: 700)
+            LinearGradient(colors: [.indigo, .cyan], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack {
+                
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.title.bold())
+                    .foregroundStyle(.white)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.white)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .foregroundStyle(.secondary)
+                            .font(.largeTitle.weight(.semibold))
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                            
+                        }
                     }
+                    
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+
+                Spacer()
+                Spacer()
+
+                Text("Score: \(score)/\(questionNumber)")
+                    .font(.title.bold())
+                    .foregroundStyle(.white)
+                Spacer()
             }
-        } detail: {
-            Text("Select an item")
+            .padding()
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text(message)
+        }
+        .alert("Game Over!", isPresented: $isGameOver) {
+            Button("New Game", action: newGame)
+        } message: {
+            Text(message)
+        }
+        
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            score += 1
+            scoreTitle = "Correct"
+            message = "You got it right!\nYour current score is \(score)."
+            
+        } else {
+            scoreTitle = "Wrong"
+            message = "You got it wrong :(\nIt was \(countries[number])'s flag.\nYour current score is \(score)."
+        }
+        questionNumber += 1
+        if questionNumber < 8 {
+            showingScore = true
+        } else {
+            message = "Your final score is \(score)/8."
+            isGameOver = true
+        }
+        
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func newGame() {
+        score = 0
+        questionNumber = 0
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
 }
 
 #Preview {
